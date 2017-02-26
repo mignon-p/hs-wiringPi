ae28f0a5260217ee9a9e21c9ed5b521baf2b2d82
Patrick Pelletier <code@funwithsoftware.org>
2017-02-26 08:19:39 +0000

    support the Pi-LITEr
    (This is just a matter of changing the order, so the LEDs appear in
    the right sequence.)

--- examples/output-example.hs
+++ examples/output-example.hs
@@ -3,15 +3,25 @@
 --   https://www.flickr.com/photos/107479024@N04/31360240974/
 -- Blinks the LEDs in sequence:
 --   https://www.flickr.com/photos/107479024@N04/32070893472/
+-- Can also be used with the Pi-LITEr:
+--   https://pinout.xyz/pinout/pi_lite_r
+--   http://store.acmeun.com/products/pi-liter-8-led-strip-for-the-raspberry-pi.html
+-- Should also work with the Ladder Board, although you'd need to
+-- change "pins" to [0..7] to get the right order:
+--   https://projects.drogon.net/the-raspberry-ladder-board/
 
 import Control.Concurrent
 import Control.Monad
 
 import System.Hardware.WiringPi
 
+-- This is the order in which the LEDs appear on the Pi-LITEr,
+-- by wiringPi pin number.
+pins = map Wpi [7, 0, 2, 1, 3, 4, 5, 6]
+
 main = do
-  forM_ [0..7] $ \x -> pinMode (Wpi x) OUTPUT
-  forever $ forM_ [0..7] $ \x -> do
-    digitalWrite (Wpi x) HIGH
+  forM_ pins $ \x -> pinMode x OUTPUT
+  forever $ forM_ pins $ \x -> do
+    digitalWrite x HIGH
     threadDelay 200000
-    digitalWrite (Wpi x) LOW
+    digitalWrite x LOW
--- examples/write-byte-example.hs
+++ examples/write-byte-example.hs
@@ -3,6 +3,12 @@
 --   https://www.flickr.com/photos/107479024@N04/31360240974/
 -- Blinks the LEDs in sequence:
 --   https://www.flickr.com/photos/107479024@N04/32070893472/
+-- Can also be used with the Pi-LITEr:
+--   https://pinout.xyz/pinout/pi_lite_r
+--   http://store.acmeun.com/products/pi-liter-8-led-strip-for-the-raspberry-pi.html
+-- Should also work with the Ladder Board, although you'd need to
+-- change "pins" to [0..7] to get the right order:
+--   https://projects.drogon.net/the-raspberry-ladder-board/
 
 import Control.Concurrent
 import Control.Monad
@@ -10,8 +16,12 @@ import Data.Bits
 
 import System.Hardware.WiringPi
 
+-- This is the order in which the LEDs appear on the Pi-LITEr,
+-- by wiringPi pin number.
+pins = [7, 0, 2, 1, 3, 4, 5, 6]
+
 main = do
-  forM_ [0..7] $ \x -> pinMode (Wpi x) OUTPUT
-  forever $ forM_ [0..7] $ \x -> do
-    digitalWriteByte $ 1 `shiftL` x
+  forM_ pins $ \x -> pinMode (Wpi x) OUTPUT
+  forever $ forM_ pins $ \x -> do
+    digitalWriteByte $ bit x
     threadDelay 200000
