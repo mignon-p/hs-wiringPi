129fcc416d083ced60789e77ba50ed010ab902a8
Patrick Pelletier <code@funwithsoftware.org>
2017-03-10 20:00:42 +0000

    gamma correction for pwm-example

--- examples/pwm-example.hs
+++ examples/pwm-example.hs
@@ -13,8 +13,8 @@ pwmPin = Wpi 1
 main = do
   pinMode pwmPin PWM_OUTPUT
   forM_ [0, 0.1 ..] $ \x -> do
-    let y = sin x
-        y' = (y + 1) / 2
+    let y = (sin x + 1) / 2
+        y' = y ** 2.8       -- gamma correction
         analog = y' * 1024
     pwmWrite pwmPin $ round analog
     threadDelay 50000
